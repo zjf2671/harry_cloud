5eed33da0cfdced8c6ecba9e8b1d3b978c94a7d5
harry <zhangjianfeng>
2019-05-07 16:39:25 +0800

    feat: add sleuth+zipkin

---
+++ cloud_server/zipkin/src/main/java/com/zjf/ZipkinApplication.java
@@ -0,0 +1,20 @@
+package com.zjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * @author Harry
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
